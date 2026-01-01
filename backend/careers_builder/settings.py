19a27d39522c141356eeac1bad94b6699cd1dde8
Dhanush Burra <dhanushburra@Dhanushs-MacBook-Air.local>
2026-01-02 01:14:07 +0530

    Improved cors code

--- backend/careers_builder/settings.py
+++ backend/careers_builder/settings.py
@@ -168,8 +168,10 @@ CORS_ALLOWED_ORIGINS = env.list(
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
+        "https://whitecarrot-assignment-three.vercel.app",  # add your Vercel frontend
     ]
 )
+
 
 CORS_ALLOW_CREDENTIALS = True
 
